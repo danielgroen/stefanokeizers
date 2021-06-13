13fe577c3e87476333dfa80a30a3eec3c964db56
danielgroen <danielgroen@users.noreply.github.com>
2021-06-13 17:18:29 +0000

    deploy: 2f1f5148bb6b14b3f946f835027882c8b3733a0e

---
+++ _nuxt/bedb909.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{150:function(n,t,e){"use strict";var c=e(41),component=Object(c.a)({},(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("Nuxt")],1)}),[],!1,null,null,null);t.a=component.exports},151:function(n,t,e){e(152),n.exports=e(153)}},[[151,5,2,6]]]);
