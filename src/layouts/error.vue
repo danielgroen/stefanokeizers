387eae847bef34941e0c4017410ec8556cbfe5b3
Daniel <groendaniel@gmail.com>
2021-06-15 20:54:57 +0200

    feat(init): 🎉 testing 404

--- src/layouts/error.vue
+++ src/layouts/error.vue
@@ -1,20 +1,15 @@
 <template>
-  <div>
-    <h1>You are in layouts/error.vue</h1>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <pre v-if="error.statusCode !== 404">{{ error }}</pre>
-    <nuxt-link to="/">Click to visit Home page</nuxt-link>
-  </div>
+  <div></div>
 </template>
 
 <script>
-export default {
-  props: {
-    error: {
-      type: [Object, Error],
-      default: () => {},
-    },
+import { useRouter, defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const router = useRouter()
+
+    router.push('/')
   },
-}
+})
 </script>
